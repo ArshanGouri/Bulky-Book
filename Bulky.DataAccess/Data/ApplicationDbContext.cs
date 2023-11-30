c2bf56827fbfe5fccd69929a88c9d135abbcf601
argou <argou@ARSHAN_GOURI>
2023-11-30 10:48:23 +0530

    Add project files.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,104 @@
+﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+                
+        }
+        //Name of Table : Categories, Category is the Model name
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);//requires for IdentityDbContext
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                  new Category
+                  {
+                      Id = 2,
+                      Name = "SciFi",
+                      DisplayOrder = 2
+                  },
+                    new Category
+                    {
+                        Id = 3,
+                        Name = "History",
+                        DisplayOrder = 3
+                    }
+                    );
+            modelBuilder.Entity<Product>().HasData(
+           new Product
+           {
+               Id = 1,
+               Title = "Serenade of the Sire",
+               Author= "Dominic Steele",
+               Description= "Immerse yourself in a tale of forbidden love and mythical creatures beneath the moonlit waves. \"Serenade of the Sirens\" follows the journey of Tristan, a young sailor who discovers a world of enchantment beneath the surface.",
+               ISBN= "978-3-456789-01-2",
+               ListPrice=19,
+               Price=90,
+               Price50=15,
+               Price100=10,
+               CategoryId=1,
+               ImageUrl=""
+           },
+           new Product
+           {
+               Id = 2,
+               Title = "Echoes in the Shadows",
+               Author = "Seraphina Nightshade",
+               Description = "Description2",
+               ISBN = "978-1-234237-11-1",
+               ListPrice = 89,
+               Price = 10,
+               Price50 = 65,
+               Price100 = 69,
+               CategoryId = 2,
+               ImageUrl=""
+
+           },
+           new Product
+           {
+               Id = 3,
+               Title = "Whispers of Eternity",
+               Author = "Seraphina Nightshade",
+               Description = "Description3",
+               ISBN = "978-1-234567-89-0",
+               ListPrice = 89,
+               Price = 10,
+               Price50 = 65,
+               Price100 = 60,
+               CategoryId = 1,
+               ImageUrl=""
+
+           },
+           new Product
+           {
+               Id = 4,
+               Title = "Chronicles of the Celestial Oracle",
+               Author = "Orion Starweaver",
+               Description = "Embark on an epic quest across celestial realms as destiny unfolds in the hands of the chosen one. \"Chronicles of the Celestial Oracle\" weaves a tapestry of magic, prophecy, and friendship as protagonist Elara discovers her role in preserving the cosmic balance.",
+               ISBN = "978-4-567890-12-3",
+               ListPrice = 89,
+               Price = 10,
+               Price50 = 65,
+               Price100 = 60,
+               CategoryId = 3,
+               ImageUrl=""// Update the CategoryId to match a valid category (e.g., 1)
+           }
+            );
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20231031103113_addForeignKeyForCategoryProductRelations.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addForeignKeyForCategoryProductRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "CategoryId", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Author", 1, "Description", "SWD999906", 99.0, 90.0, 80.0, 85.0, "Title" },
+                    { 3, "Author3", 1, "Description3", "SWD999927", 89.0, 10.0, 60.0, 65.0, "Title3" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 5, "Author3", "Description3", "SWD999927", 89.0, 10.0, 60.0, 65.0, "Title3" },
+                    { 6, "Author", "Description", "SWD999906", 99.0, 90.0, 80.0, 85.0, "Title" }
+                });
+        }
+    }
+}
